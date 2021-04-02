10c5d3c65edd1ba5f412f74a0be33a2124ffe4de
James Muriithi <muriithijames556@gmail.com>
2021-04-02 14:10:12 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3>{{goal.goal}}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggleGoal">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+  setup(){
+    //separate refs
+    // const goal = ref('Become the best developer');
+
+    // const toggleGoal = () => {
+    //   goal.value = "Retire at an early age"
+    // }
+
+    //ref object
+    const obj = ref({
+      goal: 'Become the best developer',
+    });
+
+    const toggleGoal = () => {
+      obj.value.goal = "Retire at an early age"
+    }
+
+
+    return {goal: obj, toggleGoal}
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
